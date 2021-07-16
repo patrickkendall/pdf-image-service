a760bd3902dc470aee84c89db0d207d24f43bbdf
Patrick Kendall <patrickkendall@live.com>
2021-07-16 13:57:30 -0400

    looping through images in 1 loop

--- python-docx.py
+++ python-docx.py
@@ -8,31 +8,23 @@ document = Document()
 
 document.add_picture('propertyPicsTitle.png', width=Inches(6), height=Inches(1.5))
 
-tables = document.tables
-table = document.add_table(rows=1, cols=3)
-row_cells = table.add_row().cells
-
-for i, image in enumerate(['property0.jpeg', 'property1.jpg', 'property2.jpg']):
-    paragraph = row_cells[i].paragraphs[0]
-    run = paragraph.add_run()
-    run.add_picture(image, width=Inches(1.9))
+pictures = ['property0.jpeg', 'property1.jpg', 'property2.jpg','property3.jpg', 'property4.jpg', 'property5.jpg','property2.jpg', 'property4.jpg', 'property4.jpg', 'property4.jpg', 'property4.jpg', 'property4.jpg']
 
 tables = document.tables
 table = document.add_table(rows=1, cols=3)
 row_cells = table.add_row().cells
+n = 0;
 
-for i, image in enumerate(['property3.jpg', 'property4.jpg', 'property5.jpg']):
-    paragraph = row_cells[i].paragraphs[0]
-    run = paragraph.add_run()
-    run.add_picture(image, width=Inches(1.9))
-
-tables = document.tables
-table = document.add_table(rows=1, cols=3)
-row_cells = table.add_row().cells
-
-for i, image in enumerate(['property2.jpg', 'property4.jpg', 'property4.jpg']):
-    paragraph = row_cells[i].paragraphs[0]
-    run = paragraph.add_run()
-    run.add_picture(image, width=Inches(1.9))
+for index in range(len(pictures)):
+        paragraph = row_cells[n].paragraphs[0]
+        run = paragraph.add_run()
+        run.add_picture(pictures[index], width=Inches(1.9), height=Inches(1))
+        if n % 2 == 0 and n != 0:
+            tables = document.tables
+            table = document.add_table(rows=1, cols=3)
+            row_cells = table.add_row().cells
+            n = 0;
+        else:
+            n = n + 1
 
 document.save('demo.docx')
